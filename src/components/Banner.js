a9c9909ef5eadb19d36f180d9f0c0f5834018996
Max <s6503052412161@kmutnb.ac.th>
2024-02-19 13:25:29 +0700

    update navbar and banner
     update navbar and banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -56,8 +56,8 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Welcome To my Portfolio. I'm Graduated with a bachelor's degree from </p>
+                <h1>{`Hi! I'm Max`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Welcome To my Portfolio. I'm Graduated with a bachelor's degree from King Mongkut's University of Technology North Bangkok.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,12 +50,12 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                {/* <a href="#"><img src={navIcon1} alt="" /></a> */}
+                <a href="https://www.facebook.com/inside162544/"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.instagram.com/max_rrs/"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
+                <button className="vvd"><span> Connect Here</span></button>
               </HashLink>
             </span>
           </Navbar.Collapse>
