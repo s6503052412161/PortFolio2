b1fc405ecebbd8251a85b7e633236bae40e7c70d
max <s6503052412161@kmutnb.ac.th>
2024-03-04 12:04:02 +0700

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -27,7 +27,7 @@ export const Projects = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <h2>Projects</h2>
-                  <p style={{color:"#fff"}}>I'm Do Project and Other  </p>
+                  <p style={{color:"#fff"}}>I'm do Project and Other  </p>
                   <Tab.Container id="projects-tabs" defaultActiveKey="first">
                     <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                       <Nav.Item>
