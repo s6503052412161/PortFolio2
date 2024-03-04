655f544c49aa846c12bd7bc982e7cb079b8cfa0a
Max <s6503052412161@kmutnb.ac.th>
2024-03-04 16:08:12 +0700

    update portfolio
    update portfolio

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -62,7 +62,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Max`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p style={{color:"#fff"}}>Welcome To my Portfolio. I'm Graduated with a bachelor's degree from King Mongkut's University of Technology North Bangkok.</p>
+                  <p style={{color:"black"}}>Welcome To my Portfolio. I'm Graduated with a bachelor's degree from King Mongkut's University of Technology North Bangkok.</p>
                   <button onClick={downloadCV}>Download CV <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
